6d9b475c1c58c8d2727457742e853f29acf820f6
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-02-25 20:14:45 +0500

    created botuser model

--- botapp/admin.py
+++ botapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import BotUser
 
-# Register your models here.
+
+class BotUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'username', 'user_id', 'created_at', 'updated_at')
+    search_fields = ('first_name', 'last_name', 'username', 'user_id')
+    list_filter = ('created_at', 'updated_at')
+
+
+
+admin.site.register(BotUser, BotUserAdmin)
--- botapp/models.py
+++ botapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class BotUser(models.Model):
+    user_id = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    username = models.CharField(max_length=255, null=True, blank=True)
+    language_code = models.CharField(max_length=10, default='uz')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name
